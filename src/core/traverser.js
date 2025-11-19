43112397dfd12a7ccdf5be0b60aa6ba1173e45e8
Manthan <manthanmji@gmail.com>
2025-11-19 20:57:32 +0000

    fix: default paramenter

--- src/core/traverser.js
+++ src/core/traverser.js
@@ -3,47 +3,47 @@ import State from './state.js'
  * Parses given regular expression and retures a tree.
  */
 export function triverse(state, head, states) {
-  try {
-		if(typeof(head) == 'string') {
-			if(head.length == 1) {
+	try {
+		if (typeof (head) == 'string') {
+			if (head.length == 1) {
 				state.name = head;
 			}
 			let last = state
-			head.substring(1, head.length-1).split('').forEach((child) =>{
-			  let cstate =new State(undefined, undefined, name=child) 
+			head.substring(1, head.length - 1).split('').forEach((child) => {
+				let cstate = new State(undefined, undefined, child)
 				states.push(cstate)
 				// state.connect(head, states[states.length-1])
 				last.connect(child, cstate)
 				last = cstate
 				// triverse(cstate, child)
 			})
-			const laste = states[states.length-1];
+			const laste = states[states.length - 1];
 			return [laste]
 		} else {
-			if(head.type == "or") {
+			if (head.type == "or") {
 				let last = []
-				head.children.forEach((child) =>{
+				head.children.forEach((child) => {
 					let p = triverse(state, child, states)
-					last=last.concat(p)
+					last = last.concat(p)
 				})
 				return last
-			} else if(head.type == "and") {
+			} else if (head.type == "and") {
 
-				let sg = new State(undefined, undefined, name="t")
+				let sg = new State(undefined, undefined, "t")
 				// states.push(sg) // not pushing the intermediatory state probably should delete after this function
 				let u = triverse(sg, head.children[1], states)
 				let g = triverse(state, head.children[0], states);
 				g.forEach((n) => {
-					sg.children.forEach((child)=> {
+					sg.children.forEach((child) => {
 						n.children.push(child)
 					})
 				})
 				return u;
 			} else {
 				let last = []
-				head.children.forEach((child) =>{
-					let z=triverse(state, child, states)
-					last=last.concat(z)
+				head.children.forEach((child) => {
+					let z = triverse(state, child, states)
+					last = last.concat(z)
 				})
 				last.forEach((node) => {
 					state.children.forEach((nod) => {
@@ -53,8 +53,8 @@ export function triverse(state, head, states) {
 				return last;
 			}
 		}
-  } catch (e) {
-    console.error('Error generating FA', e);
-    return null;
-  }
+	} catch (e) {
+		console.error('Error generating FA', e);
+		return null;
+	}
 }
